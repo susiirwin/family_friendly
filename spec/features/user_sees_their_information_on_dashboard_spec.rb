d2c7a845eb2c06868bb59b43c6a8528eae5a80f1
SusiIrwin <susi.irwin@gmail.com>
2016-11-03 11:16:04 -0600

    Close #11 - Dashboard page layout is laid out.

---
+++ spec/features/user_sees_their_information_on_dashboard_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe "User Dashboard" do
+  it "shows user information" do
+    stub_omniauth
+    visit '/'
+    click_on "Log in with Twitter"
+    visit '/dashboard'
+    expect(current_path).to eq('/dashboard')
+    expect(page).to have_css("h1", :text => "Horace")
+    expect(page).to have_css("h3", :text => "Number of Children age 0-1:")
+    expect(page).to have_css("h3", :text => "Number of Children age 1-2:")
+    expect(page).to have_css("h3", :text => "Number of Children age 2-3:")
+    expect(page).to have_css("h3", :text => "Number of Children age 3-4:")
+    expect(page).to have_css("h3", :text => "Number of Children age 4-5:")
+
+  end
+end
