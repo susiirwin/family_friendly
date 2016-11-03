3e77feecfd3dfafdc991959a1d0eff4bc76931d1
SusiIrwin <susi.irwin@gmail.com>
2016-11-02 21:05:19 -0600

    Complete Oauth

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'home#index'
   get '/auth/twitter', as: :twitter_login
   get '/auth/twitter/callback', to: "sessions#create"
+  delete '/logout', to: "sessions#destroy"
+
 end
