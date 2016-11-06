a1bff23fb5fa93fe2b9d0844a35ebc2943b27910
SusiIrwin <susi.irwin@gmail.com>
2016-11-05 18:28:08 -0600

    WIP - See a Business
    I need to see a business show page.

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,6 @@
+class SearchController < ApplicationController
+  def index
+    # this page needs to show all of the search results from the query - they need to be parsed and the YelpService does that for you
+    @results = 
+  end
+end
---
+++ app/models/business.rb
@@ -0,0 +1,5 @@
+class Business
+  def self.find_one
+    business = 
+  end
+end
