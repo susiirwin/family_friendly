13d24958c2deaa258c737c18aba4f335bc240095
SusiIrwin <susi.irwin@gmail.com>
2016-11-08 18:57:03 -0700

    WIP - Star Rating on New Review Form

--- app/assets/javascripts/star.js
+++ app/assets/javascripts/star.js
@@ -1,20 +1,18 @@
-$(function() {
-  $('.rating_star').click(function(){
-    var star = $(this);
-    var business_id = $(this).attr('data-business-id');
-    var stars = $(this).attr('data-stars');
+var color_stars = function(){
+  var star = $(this);
+  var business_id = $(this).attr('data-business-id');
+  var stars = $(this).attr('data-stars');
 
-    for(i=1; i<=5; i++){
-      if(i <= stars){
-        $('#' + business_id + '_' + i).addClass('on');
-      } else {
-        $('#' + business_id + '_' + i).removeClass('on');
-      }
+  for(i=1; i<=5; i++){
+    if(i <= stars){
+      $('#' + business_id + '_' + i).addClass('on');
+    } else {
+      $('#' + business_id + '_' + i).removeClass('on');
     }
+  }
+}
+$(document).ready(function() {
+  $('form .rating_star').on("click", color_stars);
+  $('form .rating_star').on("hover", color_stars);
 
-    // $.ajax({
-    //   type: "post",
-    //   url: $
-    // })
-  });
 });
