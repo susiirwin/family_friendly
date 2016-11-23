14bdd5d898fd58df7f942b41544a0dfb5b29616c
SusiIrwin <susi.irwin@gmail.com>
2016-11-23 14:52:44 -0700

    Update Business model to format address upon intake

--- app/models/business.rb
+++ app/models/business.rb
@@ -6,15 +6,16 @@ class Business < ApplicationRecord
   has_many :amenities, through: :business_amenities
 
   def self.find_all_qualifying_restaurants(params)
-    results = Yelp.client.search('Denver', { term: params[:search] })
-    require 'pry'; binding.pry
+    results = Yelp.client.search('Downtown Denver', { term: params[:search] })
+    # require 'pry'; binding.pry
     results.businesses.map do |business|
       b = Business.find_or_initialize_by(yelp_id: business.id)
       b.name = business.name
       b.yelp_id = business.id
       b.star_rating = business.rating
-      b.address = business.location.display_address
+      b.address = business.location.display_address.join(", ")
       b.phone = business.display_phone
+      b.categories = business.categories.join(', ')
       b.save!
       b
     end
